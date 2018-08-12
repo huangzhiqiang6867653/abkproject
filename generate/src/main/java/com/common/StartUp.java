6c2e8d925d2ee96afce2e5003737a12db6bc7c25
huangzhiqiang <15855172401@163.com>
2018-08-12 20:46:00 +0800

    自动生成代码功能完善

---
+++ generate/src/main/java/com/common/StartUp.java
@@ -0,0 +1,50 @@
+package com.common;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URISyntaxException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.exception.InvalidConfigurationException;
+import org.mybatis.generator.exception.XMLParserException;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+/**
+ * @author huangzhiqiang
+ *
+ * 执行生成代码工具，因为自定义了注释模版，所以需要痛殴这种方式实现
+ */
+public class StartUp {
+
+    public static void main(String[] args) throws URISyntaxException {
+        try {
+            System.out.println("--------------------start generator-------------------");
+            List<String> warnings = new ArrayList<String>();
+            boolean overwrite = true;
+            ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+            InputStream is = classloader.getResourceAsStream("generatorConfig.xml");
+            ConfigurationParser cp = new ConfigurationParser(warnings);
+            Configuration config = cp.parseConfiguration(is);
+            DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+            MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+            myBatisGenerator.generate(null);
+            System.out.println("--------------------end generator-------------------");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (InvalidConfigurationException e) {
+            e.printStackTrace();
+        } catch (XMLParserException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
---
+++ src/main/java/com/iflytek/abk/vo/BaseVo.java
@@ -0,0 +1,18 @@
+package com.iflytek.abk.vo;
+
+public class BaseVo {
+    /**
+     * 主键
+     */
+    private String id;
+
+    /**
+     * 创建时间
+     */
+    private String addTime;
+
+    /**
+     * 更新时间
+     */
+    private String updateTime;
+}
